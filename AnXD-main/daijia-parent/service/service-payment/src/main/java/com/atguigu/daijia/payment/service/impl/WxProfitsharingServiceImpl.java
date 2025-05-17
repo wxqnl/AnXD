18c74bd4e46f7dc880dd6966b18e38f0f924ecd2
yuhao <1417821628@qq.com>
2025-05-17 14:10:02 +0800

    微信支付

--- AnXD-main/daijia-parent/service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxProfitsharingServiceImpl.java
+++ AnXD-main/daijia-parent/service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxProfitsharingServiceImpl.java
@@ -1,9 +1,7 @@
 package com.atguigu.daijia.payment.service.impl;
 
-import com.atguigu.daijia.payment.mapper.PaymentInfoMapper;
 import com.atguigu.daijia.payment.service.WxProfitsharingService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
